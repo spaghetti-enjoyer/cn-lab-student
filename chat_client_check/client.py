0b5b1bc9efbc01ef3e3232e840fc8795e2faa2b8
Leo <leonidivanovzalgo@gmail.com>
2024-04-17 12:22:36 +0200

    asdfghjkjhefgbgfefggfewefvfdedfvwd

--- chat_client_check/client.py
+++ chat_client_check/client.py
@@ -1,21 +1,150 @@
+import socket
+import threading
 import argparse
+import os
 
-parser = argparse.ArgumentParser(description='Process test arguments')
-parser.add_argument('--address', type=str, help='Server IP address', default='212.132.114.68')
-parser.add_argument('--port', type=int, help='Server Port', default=5378)
+# to run from terminal: 
+# python3 client.py --address "212.132.114.68" --port 5378
 
-# you can start your client by passing the arguments --address and --port which should be
-# chat server server IP address and server port you want your client to connect to. 
-# The default values are our remote server with 212.132.114.68 for address and 5378 for port.
-# Tests will start a local server and pass its address and port as arguments to the program
-# in a form of python3 client.py --address="127.0.0.1" --port 5378
+sock = None
+username = None
+attempted_name = None
 
-args = parser.parse_args()
+SERVER_HOST = "127.0.0.1"
+SERVER_PORT = 5378
 
-SERVER_HOST = args.address
-SERVER_PORT = args.port
+def connect_to_server(host, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host_port = (host, port)
+    sock.connect(host_port)
+    print('connected', sock)
+    return sock
 
-# SERVER_HOST and SERVER_PORT contain address and port arguments
 
-print('Welcome to Chat Client. Enter your login:')
-# Please put your code in this file
+def receive_from_socket():
+    response = ""
+    while "\n" not in response:
+        response += sock.recv(1).decode("utf-8")
+
+    # print("incoming message: ", response)
+
+    return response
+
+
+def send_over_socket(data):
+    string_bytes = data.encode("utf-8")
+    bytes_len = len(string_bytes)
+    num_bytes_to_send = bytes_len
+
+    # print("to be sent:", data)
+
+    while num_bytes_to_send > 0:
+        num_bytes_to_send -= sock.send(string_bytes[bytes_len-num_bytes_to_send:])
+
+
+def send_message(message):
+    global attempted_name
+
+    # print("received from user:", message)
+
+    if message == "!quit":
+        print("exiting...")
+        sock.close()
+        os._exit(0) # no error code returned
+
+    elif message == "!who":
+        send_over_socket("LIST\n")
+    
+    elif username == None:
+        # send the username to the server and see what it spits out
+        attempted_name = message
+        print(f"attempting login as {attempted_name}")
+        send_over_socket(f"HELLO-FROM {attempted_name}\n")
+
+    elif message[0] == "@" and " " in message:
+        # splitting and sending valid message to different user
+        recipient, content = message.split(" ", 1)
+        recipient = recipient[1:] # cut off @
+        send_over_socket(f"SEND {recipient} {content}\n")
+
+    # invalid messages
+    else: 
+        send_over_socket(message)
+
+
+def receive_incoming_messages():
+    global username, sock
+    # global attempted_name
+    while True:
+        message = receive_from_socket()
+
+        if "BUSY" in message:
+            print(f"Cannot log in. The server is full!")
+            print("exiting...")
+            os._exit(0)
+        
+        elif "LIST-OK" in message:
+            message = message[8:-1] # cut off the prefix and suffix
+            names = message.split(",")
+            print(f"There are {len(names)} online:")
+            for name in names:
+                print(f"- {name}")
+
+        elif username == None and "BAD-RQST-BODY" in message:
+            print(f"Cannot log in as {attempted_name}. That username contains disallowed characters.")
+            # reconnect
+            sock.close()
+            sock = connect_to_server(SERVER_HOST, SERVER_PORT)
+
+        elif "IN-USE" in message:
+            print(f"Cannot log in as {attempted_name}. That username is already in use.")
+            # reconnect
+            sock.close()
+            sock = connect_to_server(SERVER_HOST, SERVER_PORT)
+
+        elif "HELLO" in message: # source of exploits af
+            username = attempted_name
+            print(f"Successfully logged in as {username}!")
+
+        elif "SEND-OK" in message:
+            print("The message was sent succesfully")
+
+        elif "BAD-DEST-USER" in message:
+            print("The destination user does not exist")
+
+        elif "DELIVERY" in message:
+            tag, username, content = message.split(" ", 2)
+            print(f"From {username}: {content}")
+
+        elif "BAD-RQST-HDR" in message:
+            print("Error: Unknown issue in previous message header.")
+
+        elif "BAD-RQST-BODY" in message:
+            print("Error: Unknown issue in previous message body.")
+
+        else:
+            print("idk what just happened.")
+            # os._exit(0)
+
+
+if __name__ == "__main__":
+    # deal with commandline arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--address", type=str)
+    parser.add_argument("--port", type=int)
+    args = parser.parse_args()
+
+    SERVER_HOST = args.address
+    SERVER_PORT = args.port
+
+    sock = connect_to_server(SERVER_HOST, SERVER_PORT)
+
+    print("Welcome to Chat Client. Enter your login: test")
+
+    # thread for incoming messages in the background
+    receiving = threading.Thread(target = receive_incoming_messages, daemon=True).start()
+
+    while True:
+        message = input()
+        send_message(message)
+    
--- server_check/server.py
+++ server_check/server.py
@@ -1,5 +1,148 @@
-SERVER_ADDRESS = '127.0.0.1'
-SERVER_PORT = 5378
+import socket
+import argparse
+import select
 
-print('Server is on')
-# Please put your code in this file
+# https://realpython.com/python-sockets/
+# to run from terminal:
+# python3 server.py --address "127.0.0.1" --port 5378
+
+CAPACITY = 16
+FAILED_TO_SEND = 1
+
+clients = {} # socket -> username
+
+def receive_from_socket(sock):
+    print('hafiahf: ', sock.recv(1).decode("utf-8"))
+    response = ""
+    while "\n" not in response:
+        response += sock.recv(1).decode("utf-8")
+
+    # if response == "":
+    #     print(f"Client {sock} has disconnected")
+    #     sock.close()
+    #     del clients[sock]
+    return response
+
+
+def send_over_socket(sock, data):
+
+    print("to be sent:", data)
+
+    string_bytes = data.encode("utf-8")
+    bytes_len = len(string_bytes)
+    num_bytes_to_send = bytes_len
+
+    while num_bytes_to_send > 0:
+        try:
+            num_bytes_to_send -= sock.send(string_bytes[bytes_len-num_bytes_to_send:])
+        except BrokenPipeError:
+            print(f"Client {sock} has disconnected")
+            sock.close()
+            del clients[sock]
+            return FAILED_TO_SEND
+
+
+def handle_incoming_message(client, message):
+
+    print("length:", len(clients))
+    print("connected clients:", clients.values())
+
+    if clients[client] == None:
+        if "HELLO-FROM" in message:
+
+            # try to log em in
+            if len(clients) > CAPACITY:
+                send_over_socket(client, "BUSY\n")
+                client.close()
+                del clients[client]
+                return
+            
+            header, username = message.split(" ", 1)
+            username = username[:-1] # cut off \n
+
+            if " " in username or "," in username or "\\n" in username:
+                send_over_socket(client, "BAD-RQST-BODY\n")
+            
+            elif username in list(clients.values()):
+                send_over_socket(client, "IN-USE\n")
+            
+            elif username not in clients.values():
+                send_over_socket(client, f"HELLO {username}\n")
+                clients[client] = username
+            
+        else:
+            send_over_socket(client, "BAD-RQST-HDR\n")
+
+    else:
+        print('message is: ', message)
+
+        if "HELLO-FROM" in message:
+            send_over_socket(client, "BAD-RQST-HDR\n")
+
+        if "LIST" in message:
+            res = "LIST-OK "
+            for user in clients.values():
+                res += user + ","
+            res = res[:-1] # truncate the comma at the end
+            res += "\n"
+            send_over_socket(client, res)
+
+        elif "SEND" in message:
+            header, user, body = message.split(" ", 2)
+
+            # print("to:", user)
+            # print("all users:", names_list)
+
+            if len(body) < 2:
+                send_over_socket(client, "BAD-RQST-BODY\n")
+                return
+
+            if user not in clients.values():
+                send_over_socket(client, "BAD-DEST-USER\n")
+                return
+            recipient = list(filter(lambda x: clients[x] == user, clients))[0]
+            if send_over_socket(recipient, f"DELIVERY {clients[client]} {body}\n") == FAILED_TO_SEND:
+                return
+            send_over_socket(client, "SEND-OK\n")
+
+        # I assume this is whats expected cos how else do you differentiate whats body and whats header related
+        # elif len(message) < 2: 
+        #     send_over_socket(client, "BAD-RQST-HDR\n")
+
+        else:
+            send_over_socket(client, "BAD-RQST-BODY\n")
+
+
+if __name__ == "__main__":
+
+    # deal with commandline arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--address", type=str)
+    parser.add_argument("--port", type=int)
+    args = parser.parse_args()
+
+    server_host = args.address
+    server_port = args.port
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((server_host, server_port))
+    server_socket.listen()
+
+    print("Server is on\n")
+
+    while True: 
+        sockets = [server_socket] + list(clients.keys())
+        rdlist, wrlist, exlist = select.select(sockets, [], [])
+        for socket in rdlist:
+            if socket is server_socket:
+                connection, address = server_socket.accept()
+                clients[connection] = None
+                print("new connection!")
+            else:    
+                message = receive_from_socket(socket)
+
+                print("received:", message, ".", sep="|")
+
+                # message evt handler
+                handle_incoming_message(socket, message)
